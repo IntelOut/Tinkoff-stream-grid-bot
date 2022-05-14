cc4dde186ffb0f59a4d0d0b63dd3959f7628859b
IntelOut <IntelOut@users.noreply.github.com>
2022-05-14 09:42:00 +0300

    Update Tinkoff-stream-grid-bot.py

--- Tinkoff-stream-grid-bot.py
+++ Tinkoff-stream-grid-bot.py
@@ -95,7 +95,8 @@ def main() -> int:
 
             # удаляем все открытые ордера
             account_id = share['account_id']
-            client.cancel_all_orders(account_id=account_id)
+            #тестово убираем отмену ордеров
+            #client.cancel_all_orders(account_id=account_id)
 
             # заполняем цены
             for i in range(share['number'] + 1):
